c2b4e40ad18dc6901adbaa236ba443b74c7c1bad
Кирилл <52028346+kelpasa@users.noreply.github.com>
2020-03-26 15:54:06 +0300

    Create Proof Read.py

---
+++ 6 кю/Proof Read.py
@@ -0,0 +1,16 @@
+'''
+You've just finished writing the last chapter for your novel when a virus suddenly infects your document. It has swapped the 'i's and 'e's in 'ei' words and capitalised random letters. Write a function which will:
+
+a) remove the spelling errors in 'ei' words. (Example of 'ei' words: their, caffeine, deceive, weight)
+
+b) only capitalise the first letter of each sentence. Make sure the rest of the sentence is in lower case.
+
+Example: He haD iEght ShOTs of CAffIEne. --> He had eight shots of caffeine.
+'''
+
+def proofread(string):
+    s =  string.lower().replace('ie','ei').split('. ')
+    lst = []
+    for i in s:
+        lst.append(i.capitalize())
+    return '. '.join(lst)
