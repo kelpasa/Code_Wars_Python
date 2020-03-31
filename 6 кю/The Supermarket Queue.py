e2b5f09cc14f75544a7f9a7c3a8359d671edfb24
Кирилл <52028346+kelpasa@users.noreply.github.com>
2020-03-31 13:58:51 +0300

    Create  The Supermarket Queue.py

---
+++ 6 кю/The Supermarket Queue.py
@@ -0,0 +1,13 @@
+'''
+There is a queue for the self-checkout tills at the supermarket. Your task is write a function to calculate the total time required for all the customers to check out!
+
+input
+customers: an array of positive integers representing the queue. Each integer represents a customer, and its value is the amount of time they require to check out.
+n: a positive integer, the number of checkout tills.
+'''
+def queue_time(customers, n):
+    tills = [0]*n
+    for i in customers:
+      tills[0] += i
+      tills.sort()
+    return max(tills)
