12576dd85e67b158e0512e267f1ab0c91b51192b
Кирилл <52028346+kelpasa@users.noreply.github.com>
2020-04-06 18:55:39 +0300

    Create  Sequences and Series.py

---
+++ 6 кю/Sequences and Series.py
@@ -0,0 +1,20 @@
+'''
+Have a look at the following numbers.
+
+ n | score
+---+-------
+ 1 |  50
+ 2 |  150
+ 3 |  300
+ 4 |  500
+ 5 |  750
+Can you find a pattern in it? If so, then write a function getScore(n)/get_score(n)/GetScore(n) which returns the score for any positive number n:
+
+int getScore(1) = return 50;
+int getScore(2) = return 150;
+int getScore(3) = return 300;
+int getScore(4) = return 500;
+int getScore(5) = return 750;
+'''
+def get_score(n):
+    return ((25*n)+25)*n
