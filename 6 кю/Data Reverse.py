c08e71b73bf3fb6d096a22110586739f84002710
Кирилл <52028346+kelpasa@users.noreply.github.com>
2020-04-25 15:15:07 +0300

    Create Data Reverse.py

---
+++ 6 кю/Data Reverse.py
@@ -0,0 +1,21 @@
+ '''stream of data is received and needs to be reversed.
+
+Each segment is 8 bits long, meaning the order of these segments needs to be reversed, for example:
+
+11111111  00000000  00001111  10101010
+ (byte1)   (byte2)   (byte3)   (byte4)
+should become:
+
+10101010  00001111  00000000  11111111
+ (byte4)   (byte3)   (byte2)   (byte1)
+The total number of bits will always be a multiple of 8.
+
+The data is given in an array as such:
+
+[1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0]'''
+
+import numpy as np
+from itertools import chain
+
+def data_reverse(data):
+    return list(chain(*(np.array_split(data,len(data)//8)[::-1])))
