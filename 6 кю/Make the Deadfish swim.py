e69e0228396ad137d86dea3dc793b2b2a68f8f06
Кирилл <52028346+kelpasa@users.noreply.github.com>
2020-05-02 13:57:51 +0300

    Create Make the Deadfish swim.py

---
+++ 6 кю/Make the Deadfish swim.py
@@ -0,0 +1,25 @@
+'''Write a simple parser that will parse and run Deadfish.
+
+Deadfish has 4 commands, each 1 character long:
+
+i increments the value (initially 0)
+d decrements the value
+s squares the value
+o outputs the value into the return array
+Invalid characters should be ignored.
+
+parse("iiisdoso")  ==>  [8, 64]'''
+
+def parse(data):
+    arr = []
+    x = 0
+    for i in data:
+        if i == 'i':
+            x +=1
+        elif i == 's':
+            x = x**2
+        elif i == 'd':
+            x -=1
+        elif i == 'o':
+            arr.append(x)
+    return arr
