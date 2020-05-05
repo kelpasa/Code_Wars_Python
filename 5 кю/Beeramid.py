9bfa67be691828285dbe7b93f4cca090c2f6c9b5
Кирилл <52028346+kelpasa@users.noreply.github.com>
2020-05-05 10:59:49 +0300

    Create Beeramid.py

---
+++ 5 кю/Beeramid.py
@@ -0,0 +1,30 @@
+'''
+Let's pretend your company just hired your friend from college and paid you a referral bonus. Awesome! To celebrate, you're taking your team out to the terrible dive bar next door and using the referral bonus to buy, and build, the largest three-dimensional beer can pyramid you can. And then probably drink those beers, because let's pretend it's Friday too.
+
+A beer can pyramid will square the number of cans in each level - 1 can in the top level, 4 in the second, 9 in the next, 16, 25...
+
+Complete the beeramid function to return the number of complete levels of a beer can pyramid you can make, given the parameters of:
+
+1) your referral bonus, and
+
+2) the price of a beer can
+
+For example:
+
+beeramid(1500, 2); // should === 12
+beeramid(5000, 3); // should === 16
+'''
+
+
+
+import math
+def beeramid(bonus, price):
+    if bonus <= 0:
+        return 0
+    else:
+        res = math.floor(bonus/price)
+        x = 1
+        while res >= 0:
+            res -= x**2
+            x +=1
+        return x-2
