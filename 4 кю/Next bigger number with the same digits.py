787d0a9eebab070c67981b0f76e7c280c73dac07
Кирилл <52028346+kelpasa@users.noreply.github.com>
2020-05-14 22:01:58 +0300

    Create Next bigger number with the same digits.py

---
+++ 4 кю/Next bigger number with the same digits.py
@@ -0,0 +1,33 @@
+'''
+Create a function that takes a positive integer and returns the next bigger number that can be formed by rearranging its digits. For example:
+
+12 ==> 21
+513 ==> 531
+2017 ==> 2071
+nextBigger(num: 12)   // returns 21
+nextBigger(num: 513)  // returns 531
+nextBigger(num: 2017) // returns 2071
+If the digits can't be rearranged to form a bigger number, return -1 (or nil in Swift):
+
+9 ==> -1
+111 ==> -1
+531 ==> -1
+nextBigger(num: 9)   // returns nil
+nextBigger(num: 111) // returns nil
+nextBigger(num: 531) // returns nil
+'''
+
+
+
+import itertools
+def next_bigger(n):
+    s = list(str(n))
+    for i in range(len(s)-2,-1,-1):
+        if s[i] < s[i+1]:
+            t = s[i:]
+            m = min(filter(lambda x: x>t[0], t))
+            t.remove(m)
+            t.sort()
+            s[i:] = [m] + t
+            return int("".join(s))
+    return -1
