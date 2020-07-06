11947f225599094356b8e932cbb0ce72b320e174
Кирилл <52028346+kelpasa@users.noreply.github.com>
2020-07-06 22:17:48 +0300

    Create Primorial Of a Number.py

---
+++ 6 кю/Primorial Of a Number.py
@@ -0,0 +1,27 @@
+'''
+Definition (Primorial Of a Number)
+Is similar to factorial of a number, In primorial, not all the natural numbers get multiplied, only prime numbers are multiplied to calculate the primorial of a number. It's denoted with P#.
+'''
+
+
+
+
+
+def num_primorial(n):
+    lst = [2]
+    for i in range(3, 10000, 2):
+        if (i > 10) and (i % 10 == 5):
+            continue
+        for j in lst:
+            if j * j - 1 > i:
+                lst.append(i)
+                break
+            if (i % j == 0):
+                break
+        else:
+            lst.append(i)
+    res = 1
+    for i in lst[:n]:
+        res *= i
+
+    return res
