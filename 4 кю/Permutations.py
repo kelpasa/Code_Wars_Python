a2073a06f4c6fd6392d852dc968e7312d76b72d1
Кирилл <52028346+kelpasa@users.noreply.github.com>
2020-09-05 21:32:02 +0300

    Create Permutations.py

---
+++ 4 кю/Permutations.py
@@ -0,0 +1,16 @@
+'''
+In this kata you have to create all permutations of an input string and remove duplicates, if present. This means, you have to shuffle all letters from the input in all possible orders.
+https://www.codewars.com/kata/5254ca2719453dcc0b00027d/train/python
+'''
+def permutations(string):
+    result = set([string])
+    if len(string) == 2:
+        result.add(string[1] + string[0])
+
+    elif len(string) > 2:
+        for i, c in enumerate(string):
+            for s in permutations(string[:i] + string[i + 1:]):
+                result.add(c + s)
+
+    return list(result)
+
